aae63297acfcc96258500c4191ff9a6b49fbf7d5
SoftHeroes <shubhamjobanputra@gmail.com>
2022-03-09 18:36:38 +0530

    Adding base code

---
+++ app/Containers/AppSection/Account/UI/API/Routes/GetAllAccounts.v1.private.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @apiGroup           Account
+ * @apiName            accounts
+ *
+ * @api                {GET} /v1/accounts Endpoint title here..
+ * @apiDescription     Endpoint description here..
+ *
+ * @apiVersion         1.0.0
+ * @apiPermission      none
+ *
+ * @apiParam           {String}  parameters here..
+ *
+ * @apiSuccessExample  {json}  Success-Response:
+ * HTTP/1.1 200 OK
+{
+  // Insert the response of the request here...
+}
+ */
+
+use App\Containers\AppSection\Account\UI\API\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+
+Route::get('accounts', [Controller::class, 'accounts'])
+    ->name('api_account_accounts')
+    ->middleware(['auth:api']);
+

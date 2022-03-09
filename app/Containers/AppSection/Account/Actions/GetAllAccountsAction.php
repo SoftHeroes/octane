2de96ee13e76c5073159d4ca385b334ee23fa752
SoftHeroes <shubhamjobanputra@gmail.com>
2022-03-09 19:11:58 +0530

    Adding API

--- app/Containers/AppSection/Account/Actions/GetAllAccountsAction.php
+++ app/Containers/AppSection/Account/Actions/GetAllAccountsAction.php
@@ -2,13 +2,12 @@
 
 namespace App\Containers\AppSection\Account\Actions;
 
-use App\Containers\AppSection\Account\Tasks\GetAllAccountsTask;
 use App\Ship\Parents\Actions\Action;
-use App\Ship\Parents\Requests\Request;
+use App\Containers\AppSection\Account\Tasks\GetAllAccountsTask;
 
 class GetAllAccountsAction extends Action
 {
-    public function run(Request $request)
+    public function run()
     {
         return app(GetAllAccountsTask::class)->addRequestCriteria()->run();
     }
--- app/Containers/AppSection/Account/UI/API/Controllers/Controller.php
+++ app/Containers/AppSection/Account/UI/API/Controllers/Controller.php
@@ -2,16 +2,14 @@
 
 namespace App\Containers\AppSection\Account\UI\API\Controllers;
 
-use Request;
-use Apiato\Core\Foundation\Facades\Apiato;
 use App\Ship\Parents\Controllers\ApiController;
 use App\Containers\AppSection\Account\Actions\GetAllAccountsAction;
 
 class Controller extends ApiController
 {
-    public function getAllAccounts(Request $request)
+    public function getAllAccounts()
     {
-        $accounts = Apiato::call(GetAllAccountsAction::class,[$request]);
+        $accounts = app(GetAllAccountsAction::class)->run();
         return $this->json($accounts);
     }
 }

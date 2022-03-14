0cd13af4b7132d882f9517cbbd2bd2c98e693a9c
SoftHeroes <shubhamjobanputra@gmail.com>
2022-03-14 15:43:55 +0530

    Adding mesg

--- app/Containers/AppSection/Account/Actions/GetAllAccountsAction.php
+++ app/Containers/AppSection/Account/Actions/GetAllAccountsAction.php
@@ -4,11 +4,17 @@ namespace App\Containers\AppSection\Account\Actions;
 
 use App\Ship\Parents\Actions\Action;
 use App\Containers\AppSection\Account\Tasks\GetAllAccountsTask;
+use Illuminate\Support\Facades\Log;
 
 class GetAllAccountsAction extends Action
 {
+    protected $count = 0;
+
     public function run()
     {
+
+        Log::debug("Account Call : " . $this->count++);
+
         return app(GetAllAccountsTask::class)->addRequestCriteria()->run();
     }
 }

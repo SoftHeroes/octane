fb82d020e6566433dea0188c1eac3e41f6365e66
SoftHeroes <shubhamjobanputra@gmail.com>
2022-03-14 21:53:30 +0530

    Adding date time

--- app/Containers/AppSection/Account/Actions/GetAllAccountsAction.php
+++ app/Containers/AppSection/Account/Actions/GetAllAccountsAction.php
@@ -4,6 +4,7 @@ namespace App\Containers\AppSection\Account\Actions;
 
 use App\Ship\Parents\Actions\Action;
 use App\Containers\AppSection\Account\Tasks\GetAllAccountsTask;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 
 class GetAllAccountsAction extends Action
@@ -12,9 +13,13 @@ class GetAllAccountsAction extends Action
 
     public function run()
     {
+        $start = Carbon::now();
+        $data = app(GetAllAccountsTask::class)->addRequestCriteria()->run();
+        $end = Carbon::now();
 
-        Log::debug("Account Call : " . $this->count++);
-
-        return app(GetAllAccountsTask::class)->addRequestCriteria()->run();
+        Log::debug("Start : " . $start);
+        Log::debug("end : " . $end);
+        Log::debug("Diff : " . $end->diff($start));
+        return $data;
     }
 }
